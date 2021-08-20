c334ac056210a41142c8bea5f2fe93de85a62298
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-20 11:10:50 +0530

    Refactored:Program to Filter Even Numbers and Store the result Using Stream.filter

--- NumberPlayList.java
+++ NumberPlayList.java
@@ -3,6 +3,7 @@ package com.javastreamslp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class NumberPlayList {
@@ -14,12 +15,18 @@ public class NumberPlayList {
 
         // Processing Stream
         myList.stream().forEach(n ->{
-            System.out.println(" Stream For Each Value ::" +n);
+            System.out.println(" Stream For Each Value::"+n);
         });
 
-
+        //Double Function
         Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
         List<Double> doubleList = myList.stream().map(toDoubleFunction).collect(Collectors.toList());
         System.out.println("Double List is: " + doubleList);
+
+        //Filter Even Numbers
+        Predicate<Integer> isEvenFunction = n -> n % 2 == 0;
+
+        List<Integer> evenList = myList.stream().filter(isEvenFunction).collect(Collectors.toList());
+        System.out.println("Even Numbers List : " + evenList);
     }
 }
