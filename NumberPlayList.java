eda440d6bbd74879a96af4752176ab533120097d
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-20 10:32:17 +0530

    Added:Welcome Message

---
+++ NumberPlayList.java
@@ -0,0 +1,7 @@
+package com.javastreamslp;
+
+public class NumberPlayList {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Number Play List Program Using Java Streams");
+    }
+}
