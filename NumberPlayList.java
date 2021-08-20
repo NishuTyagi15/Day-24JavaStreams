1034f6e94f92fde709d4ec040a93c5813687fccf
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-20 10:39:49 +0530

    Added:Program to Create and Iterate to Show Each Element of Stream Using forEach

--- NumberPlayList.java
+++ NumberPlayList.java
@@ -1,7 +1,18 @@
 package com.javastreamslp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberPlayList {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Number Play List Program Using Java Streams");
+        List<Integer> myList = new ArrayList<Integer>();
+        for(int i = 0; i < 4 ; i++) {
+            myList.add(i);
+        }
+
+        // Processing Stream
+        myList.stream().forEach(n ->{
+            System.out.println(" Stream For Each Value ::" +n);
+        });
     }
 }
